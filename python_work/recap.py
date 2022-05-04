f05ecdb126e42b0f683804b57a6650d9885bd847
Katy Long <katy.long@wearecodenation.com>
2022-05-04 16:16:34 +0100

    python notes addition

---
+++ python_work/recap.py
@@ -0,0 +1,148 @@
+# import random
+# # # print is how we display information in a terminal
+# # print("this is my current file")
+
+# # # strings are for representing characters
+# # print("Hello my name is Klong")
+# # print("1234")
+
+# # #This is an integer - a whole number
+# # print(1234)
+
+# # #this is a floating point - anything with a decimal
+# # print(1234.5)
+
+# # #booleans - True and False
+# # print(True)
+# # print(False)
+
+# # # none - a blank, or null type
+# # print(None)
+
+# #index position - starts its count at 0
+# print("hello"[-1])
+
+# # .notation is a way of accessing methods
+# # print("hello".upper())
+
+# # print(random.random())
+# # print(random.uniform(1,10))
+# # print(random.randint(1,10))
+
+
+# # my_name = "Klong"
+# # my_age = 30
+
+# # print(my_name)
+
+# # print("Hello my name is",my_name)
+# # print("Hello I am " + my_name)
+# # print("Hello my name is {} and I am {} years old".format(my_name,my_age))
+# # print(f"Hello my name is {my_name} and I am {my_age} years old")
+
+# # print(1+2)
+# # print(2-1)
+# # print(2*3)
+# # print(3**3)
+# # print(15/3)
+# # print(15%3)
+
+# # balance = 100
+# # amount = 50
+
+# # print(balance-amount)
+# # balance = balance - amount
+# # balance -=amount
+
+# # print(balance)
+
+# char_name = input("What is your name?    >  ")
+
+# print(f"Welcome, {char_name}")
+
+# music = "oasis"
+
+# if music == "classical":
+#     print("Oh no it's classical!")
+# elif music == "pop":
+#     print("Turn it up")
+# else:
+#     print("Yay")
+
+# num1=10
+# num2=10
+
+# if num1 > num2:
+#     print("number 1 is the bigger number")
+# elif num1<num2:
+#     print("number 2 is the bigger number")
+# else:
+#     print("number 1 and number 2 are the same")
+
+# num = 21
+# if num%7==0 and num%3==0:
+#     print("fizzbuzz")
+# elif num%3==0:
+#     print("fizz")
+# elif num%7==0:
+#     print("buzz")
+# else:
+#     print(num)
+
+# place = "Liverpool"
+# weather = "rainy"
+
+# if place=="Liverpool" and weather=="Rainy":
+#     print("of course")
+# elif place=="Liverpool" and weather=="Sunny":
+#     print("check again")
+# else:
+#     print("It's bloody windy")
+
+# day="Monday"
+# bank_holiday=True
+
+# if day=="Saturday" or day=="Sunday" or bank_holiday==True:
+#     print("Yay it's a day off")
+# else:
+#     print("Time to go to Code Nation")
+
+# def light_switch():
+#     print("Who turned out the lights?")
+
+# light_switch()
+
+# def cash_withdrawal(amount, accnum): 
+#     print(f"Withdrawing {amount} from account {accnum}")
+
+# cash_withdrawal(300, 50449921)
+# cash_withdrawal(200, 12345678)
+# cash_withdrawal(180, 99999991)
+
+# def add_up(num1,num2):
+#     return num1+num2
+
+# print(add_up(5,2))
+
+# fav_films=[
+#     "Jurassic Park",
+#     "Lost World",
+#     "Jurassic World",
+#     "Spiderman"
+# ]
+
+# for i in fav_films:
+#     print("That's a great film")
+
+# for i in range(10):
+#     print(i)
+
+# for i in range(2,12,2):
+#     print(i)
+
+num=0
+
+while num <10:
+    num +=1
+    print(num)
+
